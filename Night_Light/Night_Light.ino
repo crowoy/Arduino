cfe108aee4b3168523dd6f4ab79575dc5b938dba
Oliver Crow <crowoy@gmail.com>
2016-05-25 21:02:32 +0100

    Night Light

---
+++ Night_Light/Night_Light.ino
@@ -0,0 +1,21 @@
+int sensePin = 0;
+int ledPin = 13;
+
+void setup () {
+ analogReference(DEFAULT);
+ pinMode (ledPin, OUTPUT);
+ Serial.begin(9600);
+}
+
+void loop () {
+ Serial.println(analogRead(sensePin));
+ delay (500);
+ 
+  int val = analogRead (sensePin);
+  
+  if (val < 800) {
+    digitalWrite (ledPin, HIGH); 
+  } else {
+   digitalWrite (ledPin, LOW);
+  }
+}
