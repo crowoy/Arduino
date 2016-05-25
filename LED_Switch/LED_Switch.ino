2dd5ee3f3c976fb8b1e9f7614e4383dfe66a3aa2
Oliver Crow <crowoy@gmail.com>
2016-05-25 21:01:08 +0100

    LED On-Off Switch (with physical button)

---
+++ LED_Switch/LED_Switch.ino
@@ -0,0 +1,33 @@
+int switchPin = 8;
+int ledPin = 11;
+boolean lastButton = LOW;
+boolean currentButton = LOW;
+boolean ledOn = false;
+boolean current = LOW;
+
+void setup () {
+  pinMode (switchPin, INPUT);
+  pinMode (ledPin, OUTPUT);
+}
+
+boolean debounce (boolean lastButton) {
+  current = digitalRead (switchPin);
+  
+  if (lastButton != current) {
+  delay (5);
+  current = digitalRead (switchPin);
+  }
+  
+  return current;
+}
+
+ void loop () {
+   currentButton = debounce (lastButton);
+   
+   if (lastButton == LOW && currentButton == HIGH) {
+     ledOn = !ledOn;
+     }
+     
+     lastButton = currentButton;
+     digitalWrite (ledPin, ledOn);
+}
