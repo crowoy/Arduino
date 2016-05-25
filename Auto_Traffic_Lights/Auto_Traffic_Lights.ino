f873e687c7a5f0a9df9db6c41b2b85dbde420d77
Oliver Crow <crowoy@gmail.com>
2016-05-25 21:00:37 +0100

    Traffic Light Simulation

---
+++ Auto_Traffic_Lights/Auto_Traffic_Lights.ino
@@ -0,0 +1,24 @@
+int greenPin = 13;
+int yellowPin = 12;
+int redPin = 11;
+
+void setup () {
+pinMode (greenPin, OUTPUT);
+pinMode (yellowPin, OUTPUT);
+pinMode (redPin, OUTPUT);
+}
+
+void loop () {
+  digitalWrite (yellowPin, LOW);
+  digitalWrite (redPin, LOW);
+  digitalWrite (greenPin, HIGH);
+  delay (500);
+  digitalWrite (greenPin, LOW);
+  digitalWrite (redPin, LOW);
+  digitalWrite (yellowPin, HIGH);
+  delay (500);
+  digitalWrite (greenPin, LOW);
+  digitalWrite (yellowPin, LOW);
+  digitalWrite (redPin, HIGH);
+  delay (500);
+}
